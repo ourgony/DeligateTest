d7afa8700dbe63b65aa42cc2bc8d9b9a217657b5
ourgony <ourgony@gmail.com>
2020-06-29 11:27:59 +0900

    초기 커밋.

---
+++ DeligateTest/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeligateTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Test += Frm2_Test;
+            frm2.DeliTest += Frm2_DeliTest;
+            
+            frm2.Show();
+
+        }
+
+        private void Frm2_DeliTest(string a)
+        {
+            textBox2.Text = a;
+        }
+
+        private void Frm2_Test(object sender, string e)
+        {
+            textBox1.Text = e;
+        }
+
+        public void ActioTest(string value)
+        {
+            textBox3.Text = value;
+            textBox1.Text = value;
+            //MessageBox.Show(value);
+            return;
+        }
+    }
+}
---
+++ DeligateTest/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DeligateTest
+{
+    public partial class Form2 : Form
+    {
+        public event EventHandler<string> Test;
+
+        public delegate void delitest(string a);
+        public event delitest DeliTest;
+        public Action<string> action;
+        public Func<string, string> func;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Test?.Invoke(this, textBox1.Text);
+            DeliTest?.Invoke(textBox2.Text);
+            
+            func?.Invoke(textBox4.Text);
+
+            Form1 frm1 = new Form1();
+            action = frm1.ActioTest;
+            action(textBox3.Text);
+
+            //Action<string> action = (a) => { textBox3.Text = a; };
+
+            //this.Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
